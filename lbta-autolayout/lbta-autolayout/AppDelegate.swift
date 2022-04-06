2a080cc0e66d69be435d25df8a5c40dd7a9be1cd
Maxim Tsyganov <0xc00000f@gmail.com>
2022-04-07 02:06:51 +0300

    init project

---
+++ lbta-autolayout/lbta-autolayout/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  lbta-autolayout
+//
+//  Created by Maxim Tsyganov on 07.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.makeKeyAndVisible()
+        window.rootViewController = ViewController()
+
+        self.window = window
+
+        return true
+    }
+
+}
+
---
+++ lbta-autolayout/lbta-autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lbta-autolayout
+//
+//  Created by Maxim Tsyganov on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
